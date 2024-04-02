4c2378056dbe152df03e0e5ef7fd89440cd280a3
reshup608 <81431661+reshup608@users.noreply.github.com>
2024-04-02 11:57:21 +0530

    Update pool.js

--- routes/pool.js
+++ routes/pool.js
@@ -1,11 +1,11 @@
 var mysql=require("mysql")
 
 var pool=mysql.createConnection({
-    host:'localhost',
+    host:'bfuqmeao0rqmrq1ynjja-mysql.services.clever-cloud.com',
     port:3306,
-    user:'root',
-    password:'root123',
-    database:'pharmacy',
+    user:'uin8jw3yzbk8kek7',
+    password:'TWUq16l3l5GOoXxalzPz',
+    database:'bfuqmeao0rqmrq1ynjja',
     connectionLimit:100,
     multipleStatements:true});
 
